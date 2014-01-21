5ed256839149fdf91eb075f391a71752fc4ccdad
斬擊 <ape47.ka@gmail.com>
2014-01-21 20:51:59 +0800

    Skin the Raid Warning Frames

--- ConceptionUI DBM-Skin/skin.lua
+++ ConceptionUI DBM-Skin/skin.lua
@@ -20,7 +20,15 @@ DBT_SavedOptions['DBM'].StartColorB = 1
 DBT_SavedOptions['DBM'].EndColorR = .618 
 DBT_SavedOptions['DBM'].EndColorG = 0
 DBT_SavedOptions['DBM'].EndColorB = 0
+DBT_SavedOptions['DBM'].IconLeft = true
+DBT_SavedOptions['DBM'].IconRight = false
 DBT_SavedOptions['DBM'].FillUpBars = false
+DBM_SavedOptions.SpecialWarningPoint = 'CENTER'
+DBM_SavedOptions.SpecialWarningX = 0
+DBM_SavedOptions.SpecialWarningY = 250
+DBM_SavedOptions.LTSpecialWarningPoint = 'CENTER'
+DBM_SavedOptions.LTSpecialWarningX = 0
+DBM_SavedOptions.LTSpecialWarningY = 140
 
 
 local function SkinBar(self)
@@ -29,8 +37,7 @@ local function SkinBar(self)
 	self.bar:SetPoint('BOTTOM', self.frame, 'BOTTOM', 0, 0)
 	self.bar_backdrop = API.DropShadow(self.bar)
 	self.bar_icon1:SetSize(20, 20)
-	self.bar_icon1:ClearAllPoints()
-	self.bar_icon1:SetPoint('BOTTOMRIGHT', self.frame, 'BOTTOMLEFT', -4, 0)
+	self.bar_icon1:SetDrawLayer('ARTWORK')
 	self.bar_icon1.overlay = self.bar:CreateTexture(nil, 'OVERLAY')
 	self.bar_icon1.overlay:SetAllPoints(self.bar_icon1)
 	self.bar_icon1.overlay:SetTexture(BUTTON_OVERLAY)
@@ -40,8 +47,7 @@ local function SkinBar(self)
 	self.bar_icon1.shadow:SetPoint('BOTTOMRIGHT', self.bar_icon1, 'BOTTOMRIGHT', 4, -4)
 	self.bar_icon1.shadow:SetTexture(BUTTON_SHADOW)
 	self.bar_icon2:SetSize(20, 20)
-	self.bar_icon2:ClearAllPoints()
-	self.bar_icon2:SetPoint('BOTTOMLEFT', self.frame, 'BOTTOMRIGHT', 4, 0)
+	self.bar_icon2:SetDrawLayer('ARTWORK')
 	self.bar_icon2.overlay = self.bar:CreateTexture(nil, 'OVERLAY')
 	self.bar_icon2.overlay:SetAllPoints(self.bar_icon2)
 	self.bar_icon2.overlay:SetTexture(BUTTON_OVERLAY)
@@ -52,8 +58,8 @@ local function SkinBar(self)
 	self.bar_icon2.shadow:SetTexture(BUTTON_SHADOW)
 	self.bar_icon1:SetTexCoord(.08, .92, .08, .92)
 	self.bar_icon2:SetTexCoord(.08, .92, .08, .92)
-	self.bar_text:SetPoint('LEFT', self.bar_icon1, 'RIGHT', 4, 0)
-	self.bar_timer:SetPoint('RIGHT', self.bar_icon2, 'LEFT', -4, 0)
+	self.bar_text:SetShadowOffset(0, 0)
+	self.bar_timer:SetShadowOffset(0, 0)
 	self.bar_spark:SetTexture(nil)
 	self.skin = true
 end
@@ -78,6 +84,14 @@ local function ApplyStyle(self)
 		self.frame:SetWidth(self.owner.options.HugeWidth)
 		self.bar:SetWidth(self.owner.options.HugeWidth)
 		self.frame:SetScale(self.owner.options.HugeScale)
+		self.bar_text:ClearAllPoints()
+		self.bar_text:SetPoint('CENTER', self.frame, 'CENTER', 0, 0)
+		self.bar_timer:ClearAllPoints()
+		self.bar_timer:SetPoint('LEFT', self.bar_text, 'RIGHT', 4, 0)
+		self.bar_icon1:ClearAllPoints()
+		self.bar_icon1:SetPoint('RIGHT', self.bar_text, 'LEFT', -4, 0)
+		self.bar_icon2:ClearAllPoints()
+		self.bar_icon2:SetPoint('LEFT', self.bar_timer, 'RIGHT', 4, 0)
 	else
 		self.bar_texture:SetTexture(nil)
 		self.bar_background:SetAlpha(0)
@@ -85,11 +99,19 @@ local function ApplyStyle(self)
 		self.frame:SetWidth(self.owner.options.Width)
 		self.bar:SetWidth(self.owner.options.Width)
 		self.frame:SetScale(self.owner.options.Scale)
+		self.bar_icon1:ClearAllPoints()
+		self.bar_icon1:SetPoint('BOTTOMRIGHT', self.frame, 'BOTTOMLEFT', -4, 0)
+		self.bar_icon2:ClearAllPoints()
+		self.bar_icon2:SetPoint('BOTTOMLEFT', self.frame, 'BOTTOMRIGHT', 4, 0)
+		self.bar_text:ClearAllPoints()
+		self.bar_text:SetPoint('LEFT', self.bar_icon1, 'RIGHT', 4, 0)
+		self.bar_timer:ClearAllPoints()
+		self.bar_timer:SetPoint('RIGHT', self.bar_icon2, 'LEFT', -4, 0)
 	end
 	self.frame:Show()
 	self.frame:SetAlpha(1)
-	self.bar_text:SetFont(self.owner.options.Font, self.owner.options.FontSize)
-	self.bar_timer:SetFont(self.owner.options.Font, self.owner.options.FontSize)
+	self.bar_text:SetFont(self.owner.options.Font, self.owner.options.FontSize, 'THICKOUTLINE')
+	self.bar_timer:SetFont(self.owner.options.Font, self.owner.options.FontSize, 'THICKOUTLINE')
 	self:Update(0)
 end
 
@@ -121,3 +143,15 @@ local function ApplyBars(self, timer, id, icon, huge, small, color, isDummy)
 end
 
 hooksecurefunc(DBT, 'CreateBar', ApplyBars)
+
+
+
+local RaidNoticeAddMessage = RaidNotice_AddMessage
+function RaidNotice_AddMessage(noticeFrame, textString, colorInfo, displayTime)
+  return RaidNoticeAddMessage(noticeFrame, textString and textString:gsub(':12:12', ':0:0:0:0:64:64:5:59:5:59'), colorInfo, displayTime)
+end
+
+RaidWarningFrame.slot1:SetFont(DAMAGE_TEXT_FONT, 20, 'THICKOUTLINE')
+RaidWarningFrame.slot1:SetShadowOffset(0, 0)
+RaidWarningFrame.slot2:SetFont(DAMAGE_TEXT_FONT, 20, 'THICKOUTLINE')
+RaidWarningFrame.slot2:SetShadowOffset(0, 0)
